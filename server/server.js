d55f5c710b31956691eb16f9a603bd87a89b6da1
ThimLohse <thim.lohse@hotmail.com>
2018-11-23 23:14:31 +0100

    simple server structure

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
 import config from './../env-setup/config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+
+
+// MIDDLE WARE //
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+//app.use(express.static()) //use for hosting the build version of the application
+app.get('/', (req, res) => {
+  res.send('Hello From The Server');
+})
+
+
+
+app.listen(config.app.port, () => {console.log(`Backend listening to port: ${config.app.port}`)});
