6b2a56767c36be4407261f260c0bcaa39ce9750c
ThimLohse <thim.lohse@hotmail.com>
2018-11-24 00:00:23 +0100

    something wrong with nodemon

--- server/server.js
+++ server/server.js
@@ -2,20 +2,24 @@ import config from './env_setup/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import http from 'http';
 
 const app = express();
-
+const server = http.Server(app);
 
 // MIDDLE WARE //
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
-//app.use(express.static()) //use for hosting the build version of the application
+//app.use(express.static(path.join(__dirname, path.relative(__dirname, '../client/build')))); //use for hosting the build version of the application
 app.get('/', (req, res) => {
   res.send('Hello From The Server');
 })
 
 
+server.listen(config.app.port, () => {console.log(`Backend listening to port: ${config.app.port}`)});
 
-app.listen(config.app.port, () => {console.log(`Backend listening to port: ${config.app.port}`)});
+process.on('SIGINT', () => {
+console.log('do SIGINT');
+process.exit();
+});
