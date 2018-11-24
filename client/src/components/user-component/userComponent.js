2138366d62e2255d62954661896f69780d934c05
Li <liling@kth.se>
2018-11-24 16:31:23 +0100

    merge userComponent

--- client/src/components/user-component/userComponent.js
+++ client/src/components/user-component/userComponent.js
@@ -3,29 +3,57 @@ import style from './index.css';
 import owl from '../../owl.png'
 
 class userComponent extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      xPos: 0,
-      yPos: 0,
-      speed: 1,
-    }
+  constructor(props) {
+      super(props);
+      
+      this.state = {
+          dx: Math.random(),
+          dy: Math.random(),
+          xpos: this.getRandomInt(window.innerWidth),
+          ypos: this.getRandomInt(window.innerHeight)
+      }
   }
 
-  getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  getRandomInt(max) {
+      return Math.floor(Math.random()*Math.floor(max));
+  }
+
+  getRandomFloat() {
+      return Math.random() - 1;
+  }
+
+  componentDidMount() {
+      this.userComponentId = setInterval(
+        () => this.updatePosition(),
+        10
+      );
+  }
+
+  updatePosition() {
+      var dx = this.state.dx;
+      var dy = this.state.dy;
+      if (this.state.xpos >= window.innerWidth || this.state.xpos < 0) {
+          dx = - dx;
+      }
+      if (this.state.ypos >= window.innerHeight || this.state.ypos < 0) {
+          dy = - dy;
+      }
+      this.setState({
+          dx: dx,
+          dy: dy,
+          xpos: this.state.xpos + dx,
+          ypos: this.state.ypos + dy,
+      })
   }
 
   render() {
-    
-    var newTop = this.getRandomInt(0, 200);
-    var newLeft = this.getRandomInt(0, 500);
-
+    const pos = {
+        position: 'absolute',
+        left: this.state.xpos,
+        bottom: this.state.ypos
+    }
     return (
-
-      <div className="user-container" style={{top : newTop, left: newLeft}}>
+      <div className="user-container" style={pos}>
         <img className="user-image" src={owl} alt="user avatar"/>
         <p>{this.props.name}</p>
       </div>
