6aace75dbb3b3d6c90a9644cbd4015215ba9b721
Li <liling@kth.se>
2018-11-24 12:52:03 +0100

    resolve merge conflict in client

--- client/src/screens/room/roomScreen.js
+++ client/src/screens/room/roomScreen.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import UserComponent from '../../components/user-component/userComponent.js';
+import style from './index.css';
 
 class roomScreen extends Component {
+  
   render() {
     return (
         <UserComponent/>
+        <div className="room-screen">
+
+        </div>
     );
   }
 }
