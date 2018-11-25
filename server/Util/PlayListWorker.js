267dd5397d35a3bb1e4eb7d87ab9dbf430f57a75
Quentin LEMAIRE <lemairequentin22@gmail.com>
2018-11-25 13:49:04 +0100

    add country

--- server/Util/PlayListWorker.js
+++ server/Util/PlayListWorker.js
@@ -55,7 +55,8 @@ export class UserHandler {
         {
           name: (tempList[key].user_data.display_name == null || tempList[key].user_data.display_name.length == 25 ? 'Stranger Danger' : tempList[key].user_data.display_name),
           id: key,
-          metadata: tempList[key].metadata
+          metadata: tempList[key].metadata,
+          country: tempList[key].user_data.country
         })
     }
     return tiny_list;
