3d00b2fa631ce307e398e47d605538763f7d0446
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-11-25 11:06:50 +0100

    added pratbubbla

--- client/src/components/user-component/userComponent.js
+++ client/src/components/user-component/userComponent.js
@@ -61,8 +61,8 @@ class userComponent extends Component {
             left: this.state.xpos,
             bottom: this.state.ypos,
             backgroundColor: this.props.color, 
-            width: this.state.sizes[this.state.currentSizeIndex],
-            height: this.state.sizes[this.state.currentSizeIndex],
+            //width: this.state.sizes[this.state.currentSizeIndex],
+            //height: this.state.sizes[this.state.currentSizeIndex],
         }
     } else {
         pos = {
@@ -70,15 +70,20 @@ class userComponent extends Component {
             left: window.innerWidth-window.innerHeight*0.2 + window.innerHeight*0.05*Math.pow(-1,this.props.index),
             bottom: window.innerHeight-window.innerHeight*0.1*(this.props.index+2),
             backgroundColor: this.props.color, 
-            height: window.innerHeight*0.1,
-            width: window.innerHeight*0.1,
+            //height: window.innerHeight*0.1,
+            //width: window.innerHeight*0.1,
         }
     }
 
     return (
       <div className="user-container" style={pos}>
-      <img className="user-image" src={this.props.avatar} alt="user avatar"/>
-        <p>{this.props.name}</p>
+        <img className="user-image" src={this.props.avatar} alt="user avatar"/>
+        <h2>{this.props.name}</h2>
+        
+        <div className="user-info">
+            <div className="user-info-arrow"/>
+            <p>{this.props.name}</p> 
+        </div>
       </div>
     );
   }
